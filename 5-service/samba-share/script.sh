05fd678252e6398966c1c77a69fa4e9c2ff0c5c4
ngodat0103 <ngovuminhdat@gmail.com>
2025-03-24 22:31:40 +0700

    Add samba-mount.service

---
+++ 5-service/samba-share/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+SHARE_URL="//192.168.1.120/akira-sharing"
+MOUNT_POINT="/mnt/samba_share"
+ip_address=$(ip route get 1.1.1.1 | awk '{print $7}')
+if [[ $ip_address =~ ^192\.168\.1\.[0-9]+$ ]]; then
+    echo "At home! Mounting Samba share..."
+    sudo mount -t cifs -o username=$SMB_USERNAME,password=$SMB_PASSWORD,uid=1000,gid=1000,nofail,_netdev //192.168.1.120/akira-sharing /mnt/samba-share
+else
+    echo "Outside home! Starting VPN first..."
+    # VPN command here
+fi
