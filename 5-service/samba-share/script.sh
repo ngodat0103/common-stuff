9c7bfa682cd99d031d6f5a3eb7f0b09164799b94
ngodat0103 <ngovuminhdat@gmail.com>
2025-03-25 10:22:43 +0700

    rclone(): update cache for performance

--- 5-service/samba-share/script.sh
+++ 5-service/samba-share/script.sh
@@ -5,7 +5,7 @@ MOUNT_POINT="/mnt/samba_share"
 ip_address=$(ip route get 1.1.1.1 | awk '{print $7}')
 if [[ $ip_address =~ ^192\.168\.1\.[0-9]+$ ]]; then
     echo "At home! Mounting Samba share..."
-    sudo mount -t cifs -o username=$SMB_USERNAME,password=$SMB_PASSWORD,uid=1000,gid=1000,nofail,_netdev //192.168.1.120/akira-sharing /mnt/samba-share
+    sudo mount -t cifs -o username=$SMB_USERNAME,password=$SMB_PASSWORD,uid=1000,gid=1000 //192.168.1.120/akira-sharing /mnt/samba-share
 else
     echo "Outside home! Starting VPN first..."
     # VPN command here
